7c856edb17f334f69d22ebebd03663ac5d0a5371
Stephen Waithumbi <stephenwaithumbi14@gmail.com>
2024-10-02 06:25:02 +0300

    Done with the loops lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(friendsNames, birthdayMessage) {
+    let newArray = [];
+    let outPutMessage = "";
+    for (let i = 0; i < friendsNames.length; i++) {        
+        outPutMessage = `Thank you, ${friendsNames[i]}, for the wonderful ${birthdayMessage} gift!`;
+        newArray.push(outPutMessage);
+    }
+    return newArray;
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+function countDown(number){
+    let countDown = number;
+    while (countDown >= 0){  
+        console.log(countDown);      
+        countDown--;
+    }    
+}
+
+console.log(countDown(10));
